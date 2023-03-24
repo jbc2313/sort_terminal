61b83aed85f152d622063a721d3e3ad899a6a80c
jb corbin <101614021+jbc2313@users.noreply.github.com>
2023-03-24 12:32:04 -0500

    message is printing in center of screen

--- terminal.c
+++ terminal.c
@@ -1,4 +1,5 @@
 #include <ncurses.h>
+#include <string.h>
 
 WINDOW *create_window(int height, int width, int start_y, int start_x);
 void kill_window(WINDOW *window);
@@ -13,6 +14,10 @@ WINDOW *create_window(int height, int width, int start_y, int start_x) {
 
 };
 
+void print_center(char *string, int height, int width) {
+    mvprintw(height/2, (width-strlen(string))/2, "%s", string); 
+};
+
 void screen() {
     WINDOW *term_window;
     int start_x, start_y, width, height, row, col;
@@ -25,14 +30,14 @@ void screen() {
     keypad(stdscr, TRUE);
     start_y = (LINES - row) / 2;
     start_x = (COLS - col) / 2; 
-    printw("Press F2 to exit");
+    //printw("Press F2 to exit");
     refresh();
     term_window = create_window(row, col, start_y, start_x);
 
     while((ch = getch()) != KEY_F(2)) {
         switch(ch) {
             case 'q':
-                printw("you pressed q");
+                print_center("You endterd q, please enter F2 to quit", row, col);
                 break;
         }
     };
