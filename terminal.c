4cec9479fad7c95d1183d613350621497d5f80ed
jb corbin <101614021+jbc2313@users.noreply.github.com>
2023-03-24 10:29:08 -0500

    working on ncurses

--- terminal.c
+++ terminal.c
@@ -1,8 +1,14 @@
 #include <ncurses.h>
 
+WINDOW *create_window(int height, int width, int start_y, int start_x);
+void kill_window(WINDOW *window);
 
 void screen() {
-
+   WINDOW *term_window;
+   int start_x, start_y, width, height;
+   int ch;
+   initscr();
+   raw();
 
 
 };
