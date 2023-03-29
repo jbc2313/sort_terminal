c66be636ce73cd76a0533f67eba036d806ba8e31
jb corbin <101614021+jbc2313@users.noreply.github.com>
2023-03-28 20:51:41 -0500

    menu for algos can move up and down

--- terminal.c
+++ terminal.c
@@ -32,15 +32,50 @@ int num_algo_choices = sizeof(algo_choices) / sizeof(algo_choices[0]);
 
 void algo_selection_menu(int height, int width, int selection_size, char **choices) {
     WINDOW *algo_menu = create_window(selection_size+10, 50, height-10, width-20);
-    int i;
+    int user_selection, i;
+    int highlight = 1;
     char teststring[] = "Print this out!";
     for(i = 0; i < selection_size; i++) {
-        mvwprintw(algo_menu, i+1, 1, "%s", choices[i]);
-        
+        mvwprintw(algo_menu, i+1, 2, "%s", choices[i]); 
     }
     //mvwprintw(algo_menu, 1, 1, "%s", "PRINT"); //this is the proper way to print inside a window 1,1 is the line/col respectively
     //wprintw(algo_menu, "PRINT THIS"); // this just prints at 0,1 of window
     wrefresh(algo_menu);
+    // now need to deal with menu selection with a loop
+    
+    mvwprintw(algo_menu, highlight, 1, ">");
+    while((user_selection=wgetch(algo_menu)) != 'q') {
+        mvwprintw(algo_menu, highlight, 1, " ");
+        switch (user_selection) {
+            case 'k':
+                highlight--;
+                if(highlight==0) {
+                    highlight = selection_size;
+                }
+                break;
+            case 'j':
+                highlight++;
+                if(highlight > selection_size) {
+                    highlight = 1;
+                }
+                break;
+            case 10: //This is the ENTER key
+                if(highlight == selection_size) {
+                    // this is the exit path 
+                } else {
+                    // go into the selected item
+                    mvwprintw(algo_menu, 12, 2, "YOU DID IT");
+                    wrefresh(algo_menu);
+                }
+                break;
+        }
+        mvwprintw(algo_menu, highlight, 1, ">");
+        wrefresh(algo_menu);
+    }
+
+
+
+
 };
 
 void screen() {
