56543bc26e60dbec54b3a2b713b87fc20fe4a7ef
jb corbin <101614021+jbc2313@users.noreply.github.com>
2023-04-02 21:22:53 -0500

    working on figuring out array of strings

--- visual_sort.c
+++ visual_sort.c
@@ -2,13 +2,25 @@
 #include <string.h>
 
 /* Bubble Sort */
+
+/*
 void swap(int *x, int *y) {
     int tmp = *x;
     *x = *y;
     *y = tmp;
 }
+*/
+
+void strswap(char *x, char *y) {
+    char tmp[30];
+    strcpy(tmp, x);
+    strcpy(x, y);
+    strcpy(y, tmp);
+}
+
 
 // n is the size of the array
+/*
 void bub(int *pntr, int n) {
     int i, z;
     for (i = 0; i < n - 1; i++) {
@@ -19,21 +31,42 @@ void bub(int *pntr, int n) {
         };
     };
 }
+*/
+
+void bub(char **pntr, int array_size, int bub_iterator) {
+    int z;
+    for (z=0; z < array_size - bub_iterator - 1; z++) {
+       if(strlen(pntr[z]) > strlen(pntr[z+1])) {
+            strswap(pntr[z], pntr[z+1]);
+       }
+    }
+}
 
 void view_bubble(WINDOW *win){
     int i, x, z;
     int bub_array[] = { 12, 8, 9, 11, 6, 23, 15, 2, 7, 21};
     int bub_array_size = sizeof(bub_array) / sizeof(bub_array[0]);
-    char visual[10][25] = {"x","x","x","x","x","x","x","x","x","x"};
+    int bub_iterator = bub_array_size - 1;
+    
+    // have to figure this out
+    char visual[10][30] = {"x","x","x","x","x","x","x","x","x","x"};
+    //char *visual[30] = {"x","x","x","x","x","x","x","x","x","x"};
+    
     //make visual match bub_array with x's 
     for(z = 0; z < bub_array_size; z++){ 
         for(x = bub_array[z] - 1; x > 0; x--){
             strcat(visual[z], "x");
         }
     }
-    for(i = 0; i < bub_array_size; i++) {
-        mvwprintw(win, i+1, 1, "%s", visual[i]);
-    }
+
+    mvwprintw(win, i+1, 1, "%s", visual[i]);
+    /* while(bub_iterator > 0){ */
+    /*     for(i = 0; i < bub_array_size; i++) { */
+    /*         mvwprintw(win, i+1, 1, "%s", visual[i]); */
+    /*     } */
+    /*     bub(visual, bub_array_size, bub_iterator); */
+    /*     bub_iterator--; */
+    /* } */
     //wrefresh(win);
 }
 
