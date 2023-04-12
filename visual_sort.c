e05bc2f31f3a33386c920f51382607a25acd5d90
jb corbin <101614021+jbc2313@users.noreply.github.com>
2023-04-12 13:00:24 -0500

    added merge code

--- visual_sort.c
+++ visual_sort.c
@@ -205,11 +205,74 @@ void view_bubble(WINDOW *win){
     }
 }
 
+
+
+
+
+
 /* Merge Sort */ 
+/* Original merge and sort code */
+
+void sort(int *pntr, int low, int mid, int high) {
+    int x, y, z;
+    int nLow = mid - low + 1;
+    int nHigh = high - mid;
+    int lowArr[nLow], highArr[nHigh];
+
+    // copy to both temp arrays
+    for (x = 0; x < nLow; x++) {
+        lowArr[x] = pntr[low + x];
+    };
+    for (y = 0; y < nHigh; y++) {
+        highArr[y] = pntr[mid + 1 + y];
+    };
+    
+    // merge temp arrays back together pntr[low...high]
+    
+    // index of lowArr
+    x = 0;
+    // index of highArr
+    y = 0;
+    // index of merged arrays
+    z = low;
+
+    while (x < nLow && y < nHigh) {
+        if (lowArr[x] <= highArr[y]) {
+            pntr[z] = lowArr[x];
+            x++;
+        } else {
+            pntr[z] = highArr[y];
+            y++;
+        };
+        z++;
+    };
+
+    while (x < nLow) {
+        pntr[z] = lowArr[x];
+        x++;
+        z++;
+    };
+
+    while (y < nHigh) {
+        pntr[z] = highArr[y];
+        y++;
+        z++;
+    };
+
+};
 
 
+void merge (int *pntr, int low, int high) {
+    if ( low < high ) {
+        // (low + high)/2 will work too, below prevents overflow 
+        int mid = low + (high - low) / 2;
+        merge(pntr, low, mid);
+        merge(pntr, mid + 1, high);
+        sort(pntr, low, mid, high);
+
+    }
+};
 
 
-
-
+/* END OF MERGE */
 
